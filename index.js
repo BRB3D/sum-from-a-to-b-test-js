b9a31d700314d17338a7029a3c49a6b7eb3adbfb
BRB3D <samuraix221@hotmail.com>
2021-10-29 22:29:28 -0400

    successful recursive function for values with base case and recursive case

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
-function sum(fromN, toN) {
+let sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  let result = fromN;
+  if (fromN !== toN) {
+    return result += sum(fromN + 1, toN);
+  }
+  return result;
+};
+
+console.log(sum(3, 7));
 
 module.exports = sum;
